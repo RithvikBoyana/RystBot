92224cdc39145a380cf44464f02815f109f74927
Rithvik Boyana <rithvikboyana005@gmail.com>
2022-07-08 00:03:32 -0500

    Custom Cmds

--- bot.py
+++ bot.py
@@ -36,6 +36,11 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.reply(f'{round(client.latency * 1000)}ms')
 
+@client.command(name="npc", help="Returns Omni")
+async def ping(ctx, member=627964932394188851):
+    m = commands.MemberConverter().convert(ctx, member)
+    await ctx.reply(f'{m} is an npc')
+
 @client.command(name="clear", aliases=["c"], help="Clears a specified number of messages in a channel or 10 by default")
 @commands.has_permissions(manage_messages = True)
 async def clear(ctx, amount):
